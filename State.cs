6b8ed36cd75ec9143511ef4a4f4fc8b5848e7578
Matthew Crowder <mcrowder65@gmail.com>
2015-11-18 14:07:28 -0700

    added state.cs class, includes a map, lowerbound, and best solution so far.

---
+++ State.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSP
+{
+    class State
+    {
+        double[,] map;
+        double LB;
+        double BSSF;
+        public State() { }
+        public State(double[,] map)
+        {
+            this.map = map;
+        }
+        public void setMap(double[,] temp)
+        {
+            map = temp;
+        }
+        public void setPoint(int x, int y, double value)
+        {
+            map[x, y] = value;
+        }
+        public double getPoint(int x, int y)
+        {
+            return map[x, y];
+        }
+        public double[,] getMap()
+        {
+            return map;
+        }
+        public void setLB(double temp)
+        {
+            LB = temp;
+        }
+        public double getLB()
+        {
+            return LB;
+        }
+        public void setBSSF(double temp)
+        {
+            BSSF = temp;
+        }
+        public double getBSSF()
+        {
+            return BSSF;
+        }
+
+    }
+}
